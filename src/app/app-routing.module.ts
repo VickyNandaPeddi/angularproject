8fd51ccf43c7344c3eff30319bcdb5dc89c9e182
NandaKishore.Peddi <vickypeddi9@gmail.com>
2022-01-29 21:15:08 +0530

    FirstCommit For all Files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {EmployeeloginComponent} from "./Component/employeelogin/employeelogin.component";
+import {CustomerloginComponent} from "./Component/customerlogin/customerlogin.component";
+import {HomepageComponent} from "./Component/homepage/homepage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomepageComponent, pathMatch: "full"
+  },
+
+  {
+    path: "employee", component: EmployeeloginComponent, pathMatch: "full"
+  },
+
+  {
+    path: "customer", component: CustomerloginComponent, pathMatch: "full"
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './Component/homepage/homepage.component';
+import { EmployeeloginComponent } from './Component/employeelogin/employeelogin.component';
+import { CustomerloginComponent } from './Component/customerlogin/customerlogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    EmployeeloginComponent,
+    CustomerloginComponent
   ],
   imports: [
     BrowserModule,
