ff4cea0267f297e3eeed08ecfbc0a96efebd334a
NandaKishore.Peddi <vickypeddi9@gmail.com>
2022-02-08 10:35:06 +0530

    yrt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,51 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {EmployeeloginComponent} from "./Component/employeelogin/employeelogin.component";
 import {CustomerloginComponent} from "./Component/customerlogin/customerlogin.component";
-import {HomepageComponent} from "./Component/homepage/homepage.component";
+import {DefaultComponent} from "./Component/homepage/default/default.component";
+import {HistoricalbackgroundComponent} from "./Component/homepage/historicalbackground/historicalbackground.component";
+import {InfrastructureComponent} from "./Component/homepage/manufacturingunit/infrastructure/infrastructure.component";
+import {EquipmentComponent} from "./Component/homepage/manufacturingunit/equipment/equipment.component";
+import {ProductrangeComponent} from "./Component/homepage/manufacturingunit/productrange/productrange.component";
+import {TechnicalcapabilitesComponent} from "./Component/homepage/technicalcapabilites/technicalcapabilites.component";
+import {RecognitionsApprovalsComponent} from "./Component/homepage/recognitions-approvals/recognitions-approvals.component";
+import {ContentComponent} from "./Component/homepage/content/content.component";
+import {ContactdetailsComponent} from "./Component/homepage/contactdetails/contactdetails.component";
 
 const routes: Routes = [
   {
-    path: "", component: HomepageComponent, pathMatch: "full"
+    path: "", component: DefaultComponent
+    , children: [
+
+      {
+        path: "", component: ContentComponent
+      },
+      {
+        path: "history", component: HistoricalbackgroundComponent
+      },
+      {
+        path: "manufacuringunit",
+        children: [
+          {
+            path: "", component: InfrastructureComponent
+          },
+          {
+            path: "equippemnt", component: EquipmentComponent
+          },
+          {
+            path: "productsrange", component: ProductrangeComponent
+          }
+        ]
+      },
+      {
+        path: "technicalcapabilities", component: TechnicalcapabilitesComponent
+      },
+      {
+        path: "recognitionsapprovals", component: RecognitionsApprovalsComponent
+      },
+      {
+        path: "contactdetails", component: ContactdetailsComponent
+      }
+    ]
   },
 
   {
@@ -15,7 +55,8 @@ const routes: Routes = [
 
   {
     path: "customer", component: CustomerloginComponent, pathMatch: "full"
-  },
+  }
+  ,
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomepageComponent } from './Component/homepage/homepage.component';
-import { EmployeeloginComponent } from './Component/employeelogin/employeelogin.component';
-import { CustomerloginComponent } from './Component/customerlogin/customerlogin.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {EmployeeloginComponent} from './Component/employeelogin/employeelogin.component';
+import {CustomerloginComponent} from './Component/customerlogin/customerlogin.component';
+
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { DefaultComponent } from './Component/homepage/default/default.component';
+import { HistoricalbackgroundComponent } from './Component/homepage/historicalbackground/historicalbackground.component';
+import { InfrastructureComponent } from './Component/homepage/manufacturingunit/infrastructure/infrastructure.component';
+import { EquipmentComponent } from './Component/homepage/manufacturingunit/equipment/equipment.component';
+import { ProductrangeComponent } from './Component/homepage/manufacturingunit/productrange/productrange.component';
+import { RecognitionsApprovalsComponent } from './Component/homepage/recognitions-approvals/recognitions-approvals.component';
+import { ContentComponent } from './Component/homepage/content/content.component';
+import { TechnicalcapabilitesComponent } from './Component/homepage/technicalcapabilites/technicalcapabilites.component';
+import { ContactdetailsComponent } from './Component/homepage/contactdetails/contactdetails.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
     EmployeeloginComponent,
-    CustomerloginComponent
+    CustomerloginComponent,
+    DefaultComponent,
+    HistoricalbackgroundComponent,
+    InfrastructureComponent,
+    EquipmentComponent,
+    ProductrangeComponent,
+    RecognitionsApprovalsComponent,
+    ContentComponent,
+    TechnicalcapabilitesComponent,
+    ContactdetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
