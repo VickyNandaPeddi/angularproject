02d68a0737bbc864bf9113d5adc3a6da48a87b95
NandaKishore.Peddi <vickypeddi9@gmail.com>
2022-02-10 09:51:06 +0530

    created customer register component

--- src/app/Component/customerlogin/customerregister/customerregister.component.ts
+++ src/app/Component/customerlogin/customerregister/customerregister.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Customer} from "../../../Model/Customer";
 
 @Component({
   selector: 'app-customerregister',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerregisterComponent implements OnInit {
 
+  // @ts-ignore
+  customer:Customer;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
